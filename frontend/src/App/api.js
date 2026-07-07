ead199ac460c5a6732be1cc6e648f1b3e63dd34e
SumanrajBera <srbera17@gmail.com>
2026-07-07 12:01:22 +0530

    Completed the API with interceptor

--- frontend/src/App/api.js
+++ frontend/src/App/api.js
@@ -5,8 +5,29 @@ const api = axios.create({
     withCredentials: true
 })
 
-// api.interceptors.response.use(
-//     (response)
-// )
+api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config
+
+        if (
+            error.response?.status === 409 &&
+            !originalRequest._retry &&
+            originalRequest.url !== "/api/auth/me"
+        ) {
+            originalRequest._retry = true;
+
+            try {
+                await api.get("/api/auth/refresh-token");
+                return api(originalRequest);
+            } catch (err) {
+                window.location.href = "/login";
+                return Promise.reject(err);
+            }
+        }
+
+        return Promise.reject(error)
+    }
+)
 
 export default api;
